61cf5c1c56d71a80378c04691530d2b586ad609e
MIGUEL JUNIOR MERCADO CASTAÑOS <20192366@unapec.edu.do>
2021-09-24 21:37:23 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace PushPull
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox = new System.Windows.Forms.TextBox();
+			this.writeFile = new System.Windows.Forms.Button();
+			this.loadFile = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBox
+			// 
+			this.textBox.Location = new System.Drawing.Point(34, 78);
+			this.textBox.Multiline = true;
+			this.textBox.Name = "textBox";
+			this.textBox.Size = new System.Drawing.Size(481, 484);
+			this.textBox.TabIndex = 0;
+			this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+			// 
+			// writeFile
+			// 
+			this.writeFile.Location = new System.Drawing.Point(34, 21);
+			this.writeFile.Name = "writeFile";
+			this.writeFile.Size = new System.Drawing.Size(164, 41);
+			this.writeFile.TabIndex = 1;
+			this.writeFile.Text = "Generar";
+			this.writeFile.UseVisualStyleBackColor = true;
+			this.writeFile.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// loadFile
+			// 
+			this.loadFile.Location = new System.Drawing.Point(568, 21);
+			this.loadFile.Name = "loadFile";
+			this.loadFile.Size = new System.Drawing.Size(176, 41);
+			this.loadFile.TabIndex = 2;
+			this.loadFile.Text = "Cargar";
+			this.loadFile.UseVisualStyleBackColor = true;
+			this.loadFile.Click += new System.EventHandler(this.loadFile_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(568, 78);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 72;
+			this.dataGridView1.RowTemplate.Height = 31;
+			this.dataGridView1.Size = new System.Drawing.Size(718, 484);
+			this.dataGridView1.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1318, 588);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.loadFile);
+			this.Controls.Add(this.writeFile);
+			this.Controls.Add(this.textBox);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox;
+		private System.Windows.Forms.Button writeFile;
+		private System.Windows.Forms.Button loadFile;
+		private System.Windows.Forms.DataGridView dataGridView1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PushPull
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+			try
+			{
+				string path = @"C:\Users\mjmer\Documents\Calc01";
+				string fileName = "Datos.txt";
+				string contents = textBox.Text;
+				File.WriteAllText(path + fileName, contents);
+			}
+			catch (Exception x)
+			{
+
+				MessageBox.Show("Ooops" + x.Message); 
+			}
+			
+			
+		}
+
+		private void loadFile_Click(object sender, EventArgs e)
+		{
+			string line;
+			DataTable dt = new DataTable();
+			dt.Clear();
+			dt.Columns.Add("Matricula");
+			dt.Columns.Add("Nombre");
+			dt.Columns.Add("Carrera");
+
+
+			// abre dialogo para leer el archivo
+
+			OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+			// Set filter options and filter index.
+			openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+			openFileDialog1.FilterIndex = 1;
+
+			//openFileDialog1.Multiselect = true;
+
+			// Call the ShowDialog method to show the dialog box.
+			openFileDialog1.ShowDialog();
+
+			string archivo = openFileDialog1.FileName;
+
+			// lee linea a linea 
+
+
+			System.IO.StreamReader file = new System.IO.StreamReader(archivo);
+			while ((line = file.ReadLine()) != null)
+			{
+				string[] registro = line.Split(',');
+				DataRow fila = dt.NewRow();
+				fila["Matricula"] = registro[0];
+				fila["Nombre"] = registro[1];
+				fila["Carrera"] = registro[2];
+				dt.Rows.Add(fila);
+			}
+
+			file.Close();
+			dataGridView1.DataSource = dt;
+			dataGridView1.Refresh();
+		}
+
+		private void textBox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
